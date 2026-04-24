280d5d1f7ea794d818aa9c5ea96ae9135166dc09
NusratJahanShamima <nusratjahanshamima9@gmail.com>
2026-04-24 08:19:20 +0600

    fix: uploads static path for railway

--- server/server.js
+++ server/server.js
@@ -6,24 +6,23 @@ const cors = require('cors');
 const app = express();
 
 // middleware
-app.use(cors({
-    origin: "*"
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // static frontend
 app.use(express.static(path.join(__dirname, '../public')));
 
-//  routes
+// routes
 app.use('/api/messages', require('./routes/messages'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 
-//  image folder
-app.use('/uploads', express.static('uploads'));
-// server start 
+//  FIXED uploads path
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
